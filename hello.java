a82ac646cc0b07ca403541c5fab6371e01f5b88f
Mahesh <mahesh@gmail.com>
2017-06-07 09:58:46 +0530

    initial commit

---
+++ hello.java
@@ -0,0 +1,12 @@
+public class hello
+{
+public static void main(String[] args)
+{
+	System.out.println("Hello Mahesh...How are you...");
+
+	for(int i=0; i<10; i++)
+	{
+		System.out.println("Mahesh and Taru");
+	}
+}
+}
